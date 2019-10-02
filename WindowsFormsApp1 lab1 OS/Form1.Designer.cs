cc128cfcb9ed032ea9c2038762829e08ac56297a
Xeej <55625846+Xeej@users.noreply.github.com>
2019-10-02 20:58:19 +0400

    c#

---
+++ WindowsFormsApp1 lab1 OS/Form1.Designer.cs
@@ -0,0 +1,363 @@
+﻿namespace WindowsFormsApp1_lab1_OS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Имя компьютера";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(166, 29);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(111, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Имя пользователя";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(166, 55);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(111, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Системный адрес";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 110);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(85, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Адрес Windows";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 136);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(67, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Temp адрес";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(166, 81);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(165, 20);
+            this.textBox3.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(166, 107);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(185, 20);
+            this.textBox4.TabIndex = 8;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(166, 133);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(225, 20);
+            this.textBox5.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 162);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(91, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Версия Windows";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(166, 159);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.ReadOnly = true;
+            this.textBox6.Size = new System.Drawing.Size(111, 20);
+            this.textBox6.TabIndex = 11;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 188);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(138, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Размер в FullScreen Х и У";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(166, 185);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.ReadOnly = true;
+            this.textBox7.Size = new System.Drawing.Size(111, 20);
+            this.textBox7.TabIndex = 13;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(2, 323);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(148, 23);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "button";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(166, 323);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(141, 50);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "Рандом цвет кнопки RGB Цвет:";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 211);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(98, 13);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "Системное время";
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(166, 211);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.ReadOnly = true;
+            this.textBox8.Size = new System.Drawing.Size(111, 20);
+            this.textBox8.TabIndex = 17;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(2, 350);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(148, 23);
+            this.button3.TabIndex = 18;
+            this.button3.Text = "Смена кнопок мыши";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(12, 235);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(115, 13);
+            this.label9.TabIndex = 19;
+            this.label9.Text = "Индикатор батареи - ";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(12, 257);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(41, 13);
+            this.label10.TabIndex = 20;
+            this.label10.Text = "label10";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(15, 289);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(14, 13);
+            this.label11.TabIndex = 21;
+            this.label11.Text = "Х";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(112, 289);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(15, 13);
+            this.label12.TabIndex = 22;
+            this.label12.Text = "У";
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(39, 286);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(58, 20);
+            this.textBox9.TabIndex = 23;
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(133, 286);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(58, 20);
+            this.textBox10.TabIndex = 24;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(202, 286);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(116, 23);
+            this.button4.TabIndex = 25;
+            this.button4.Text = "Переместить мышь";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(461, 392);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Lab1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ WindowsFormsApp1 lab1 OS/Form1.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using static System.Random;
+
+
+
+namespace WindowsFormsApp1_lab1_OS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //format getnamecomputer
+        enum COMPUTER_NAME_FORMAT
+        {
+            ComputerNameNetBIOS,
+            ComputerNameDnsHostname,
+            ComputerNameDnsDomain,
+            ComputerNameDnsFullyQualified,
+            ComputerNamePhysicalNetBIOS,
+            ComputerNamePhysicalDnsHostname,
+            ComputerNamePhysicalDnsDomain,
+            ComputerNamePhysicalDnsFullyQualified
+        }
+        const uint SPI_GETCURSORSHADOW = 0x101A;
+        const uint SPI_SETCURSORSHADOW = 0x101B;
+        const int COLOR_WINDOW = 5;
+
+        //структура батареи
+        private enum ACLineStatus : byte
+        {
+            Offline = 0,
+            Online = 1,
+            Unknown = 255
+        }
+
+        private enum BatteryFlag : byte
+        {
+            High = 1,
+            Low = 2,
+            Critical = 4,
+            charging = 8,
+            Charging = 9,
+            NoSystemBattery = 128,
+            Unknown = 255
+        }
+
+        private struct SystemPowerStatus
+        {
+            public ACLineStatus LineStatus;
+            public BatteryFlag flgBattery;
+            public Byte BatteryLifePercent;
+            public Byte Reserved1;
+            public Int32 BatteryLifeTime;
+            public Int32 BatteryFullLifeTime;
+        }
+
+
+
+        //Стуктура получения информации операционной системы
+        [StructLayout(LayoutKind.Sequential)]
+        public class OSVersionInfo
+        {
+            public uint dwOSVersionInfoSize;
+            public uint dwMajorVersion;
+            public uint dwMinorVersion;
+            public uint dwBuildNumber;
+            public uint dwPlatformId;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 128)]
+            public String szCSDVersion;
+        }
+        //формат метриков
+        public enum SystemMetric
+        {
+            SM_CXFULLSCREEN = 16,                  
+            SM_CYFULLSCREEN = 17
+        }
+
+        //cтруктура указателя мыши лучше свой
+        [StructLayout(LayoutKind.Sequential)]
+        public struct POINT
+        {
+            public int X;
+            public int Y;
+
+            public POINT(int x, int y)
+            {
+                this.X = x;
+                this.Y = y;
+            }
+
+            public static implicit operator System.Drawing.Point(POINT p)
+            {
+                return new System.Drawing.Point(p.X, p.Y);
+            }
+
+            public static implicit operator POINT(System.Drawing.Point p)
+            {
+                return new POINT(p.X, p.Y);
+            }
+        }
+
+        //подключение библиотеки имя компьютера
+        [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        static extern bool GetComputerNameEx(COMPUTER_NAME_FORMAT NameType,StringBuilder lpBuffer, ref uint lpnSize);
+        //подключение библиотеки имя пользователя
+        [DllImport("advapi32.dll", SetLastError = true)]
+        static extern bool GetUserName(System.Text.StringBuilder sb, ref Int32 length);
+        //подключение библиотеки адрес системной директории
+        [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        static extern uint GetSystemDirectory([Out] StringBuilder lpBuffer, uint uSize);
+        //подключение библиотеки адрес системной директории
+        [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        static extern uint GetWindowsDirectory(StringBuilder lpBuffer,uint uSize);
+        //подключение библиотеки адрес каталога временных файлов
+        [DllImport("kernel32.dll")]
+        static extern uint GetTempPath(uint nBufferLength,[Out] StringBuilder lpBuffer);
+        //подключение библиотеки версия винды
+        [DllImport("kernel32.dll", CharSet = CharSet.Ansi)]
+        public static extern bool GetVersionEx([In, Out] OSVersionInfo info);
+        //подключение библиотеки метрик
+        [DllImport("user32.dll")]
+        static extern int GetSystemMetrics(SystemMetric smIndex);
+        //подключение библиотеки параметров
+        [DllImport("user32.dll", EntryPoint = "SystemParametersInfo")]
+        public static extern bool SystemParametersInfo(uint uiAction, uint uiParam, ref uint pvParam, uint fWinIni);
+        [DllImport("user32.dll", EntryPoint = "SystemParametersInfo", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern bool SystemParametersInfoSet(uint uiAction, uint uiParam, uint pvParam, uint fWinIni);
+        //подключение библиотеки изменения цвета
+        [DllImport("user32.dll")]
+        static extern uint GetSysColor(int nIndex);
+        [DllImport("user32.dll")]
+        static extern bool SetSysColors(int cElements, int[] lpaElements,uint[] lpaRgbValues);
+        //подключение библиотеки времени
+        [StructLayout(LayoutKind.Sequential)]
+        public struct SYSTEMTIME
+        {
+            public short wYear;
+            public short wMonth;
+            public short wDayOfWeek;
+            public short wDay;
+            public short wHour;
+            public short wMinute;
+            public short wSecond;
+            public short wMilliseconds;
+        }
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool SetSystemTime(ref SYSTEMTIME time);
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool GetSystemTime(ref SYSTEMTIME time);
+
+        [DllImport("kernel32.dll")]
+        static extern bool GetSystemPowerStatus(out SystemPowerStatus sps);
+
+        [DllImport("user32.dll")]
+        static extern bool SwapMouseButton(bool fSwap);
+
+
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool GetCursorPos(out POINT lpPoint);
+
+        [DllImport("user32.dll")]
+        static extern bool SetCursorPos(int x, int y);
+
+        bool fSwap = false;
+
+
+        
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            StringBuilder name = new StringBuilder(260);
+            uint size = 260;
+            GetComputerNameEx(COMPUTER_NAME_FORMAT.ComputerNamePhysicalDnsHostname, name, ref size);
+            textBox1.Text = name.ToString();
+
+            int nsize = 64;
+            GetUserName(name, ref nsize);
+            textBox2.Text = name.ToString();
+
+            
+            StringBuilder sbSystemDir = new StringBuilder(256);
+            size = GetSystemDirectory(sbSystemDir, 256);
+            textBox3.Text = sbSystemDir.ToString();
+
+            nsize= 255;
+            StringBuilder sb = new StringBuilder(nsize);
+            int len = (int)GetWindowsDirectory(sb, size);
+            textBox4.Text = sb.ToString(0, len);
+
+            size = 255;
+            GetTempPath(size, sb);
+            textBox5.Text = sb.ToString();
+
+            OSVersionInfo version = new OSVersionInfo();
+            version.dwOSVersionInfoSize = (uint)Marshal.SizeOf(version);
+            GetVersionEx(version);
+            textBox6.Text = version.dwMajorVersion.ToString()+'.'+version.dwMinorVersion.ToString() ;
+
+            int x, y;
+            x = GetSystemMetrics(SystemMetric.SM_CXFULLSCREEN);
+            y = GetSystemMetrics(SystemMetric.SM_CYFULLSCREEN);
+            textBox7.Text = x.ToString() + 'x' + y.ToString();
+
+            uint pvParam=0;
+            SystemParametersInfo(SPI_GETCURSORSHADOW, 0, ref pvParam, 0);
+            if (pvParam == 1)
+                button1.Text = "Отключить тень курсора";
+            else
+                button1.Text = "Включить тень курсора";
+
+            button2.Text = "Цвет COLOR_WINDOW:" + GetSysColor(COLOR_WINDOW).ToString("X2");
+
+
+            SYSTEMTIME time = new SYSTEMTIME();
+            //получаем текущее время
+            GetSystemTime(ref time);
+            textBox8.Text = time.wDay.ToString() + "." + time.wMonth.ToString("x2") + "." + time.wYear.ToString()+"   "+time.wHour.ToString()+":"+time.wMinute.ToString();
+
+
+
+            SystemPowerStatus SPS = new SystemPowerStatus(); ;
+            GetSystemPowerStatus(out SPS);
+            label9.Text = "Заряд батареи - "+SPS.flgBattery.ToString();
+
+
+        }
+        
+        private void Button1_Click(object sender, EventArgs e)
+        {
+
+            uint pvParam = 0;
+            SystemParametersInfo(SPI_GETCURSORSHADOW, 0, ref pvParam, 0);
+            if (pvParam == 1)
+                SystemParametersInfoSet(SPI_SETCURSORSHADOW, 0, 0, 0); 
+            else
+                SystemParametersInfoSet(SPI_SETCURSORSHADOW, 0, 1, 0); 
+             pvParam = 0;
+            SystemParametersInfo(SPI_GETCURSORSHADOW, 0, ref pvParam, 0);
+            if (pvParam == 1)
+                button1.Text = "Отключить тень курсора";
+            else
+                button1.Text = "Включить тень курсора";
+
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+
+           // uint color=GetSysColor(COLOR_WINDOW);
+            uint r = 0xFFFFFF;
+            int[] el = {COLOR_WINDOW};
+            uint[] cl = {r};
+            SetSysColors(1, el, cl);
+            
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            fSwap = !fSwap;
+            SwapMouseButton(fSwap);
+            
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            POINT p;
+            GetCursorPos(out p);
+
+            label10.Text = "Позиция мыши Х.У:       " + p.X.ToString() + "." + p.Y.ToString();
+             
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+                        SetCursorPos(Convert.ToInt32(textBox9.Text.ToString()), Convert.ToInt32(textBox10.Text.ToString()));
+        }
+    }
+}
